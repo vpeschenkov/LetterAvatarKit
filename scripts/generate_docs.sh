38aa70a39107151884ecb5283da7c4636488d876
Viktor Peschenkov <v.peschenkov@gmail.com>
2026-06-16 00:39:41 +0200

    Add Jazzy documentation workflow

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-jazzy \
+set -e
+
+bundle exec jazzy \
   -x USE_SWIFT_RESPONSE_FILE=NO \
   --clean \
   --author Victor Peschenkov \
-  --author_url hhttps://github.com/vpeschenkov \
+  --author_url https://github.com/vpeschenkov \
   --github_url https://github.com/vpeschenkov/LetterAvatarKit \
   --include=/*/LetterAvatar*.*,/*/Extensions/UIImage* \
   --skip-undocumented \
