98ddb74ed4d58de8004d3c4c82c4a3e5e6e9e4d5
Viktor Peschenkov <v.peschenkov@gmail.com>
2026-06-16 00:59:04 +0200

    ci: fix Jazzy xcodebuild arguments

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -3,7 +3,7 @@
 set -e
 
 bundle exec jazzy \
-  -x -project,LetterAvatarKit.xcodeproj,-scheme,LetterAvatarKit,-sdk,iphonesimulator,CODE_SIGNING_ALLOWED=NO,USE_SWIFT_RESPONSE_FILE=NO \
+  -x -project,LetterAvatarKit.xcodeproj,-scheme,LetterAvatarKit,-sdk,iphonesimulator,CODE_SIGNING_ALLOWED=NO,ARCHS=arm64 \
   --clean \
   --author "Victor Peschenkov" \
   --author_url https://github.com/vpeschenkov \
